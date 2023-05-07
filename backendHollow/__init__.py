36ba53b57979d025edecd84c7fa8e393cda264d6
Sherman <shermanperez0@gmail.com>
2023-05-07 18:02:37 -0400

    secret key implemented

--- backendHollow/__init__.py
+++ backendHollow/__init__.py
@@ -3,12 +3,14 @@ from flask_pymongo import PyMongo
 from flask_cors import CORS
 from flask_bcrypt import Bcrypt
 from datetime import timedelta
+import os
 
 app = Flask(__name__)
 app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(days=30)
 app.config['SESSION_COOKIE_SECURE'] = True
 CORS(app, supports_credentials=True)
-app.secret_key = 'de0b6d1205e578d7d79857e211e48182f8167878f4f6ad4b8cf6a7b447cab84c'
+SECRET_KEY = os.urandom(32)
+app.config['SECRET_KEY'] = SECRET_KEY
 app.config['MONGO_URI'] = "mongodb+srv://juokx1:ivhcJlPAiN1QnVsj@wikisherman.ewhecfi.mongodb.net/hollowDB?retryWrites=true&w=majority"
 app.config['WTF_CSRF_ENABLED'] = False
 mongo = PyMongo(app)
