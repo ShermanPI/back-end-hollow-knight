68410d3f2153e7ef58b752c1c5b31a7d87c52902
Sherman <shermanperez0@gmail.com>
2023-05-07 18:43:05 -0400

    Access-Control-Allow-Credentials for cookies

--- backendHollow/routes.py
+++ backendHollow/routes.py
@@ -1,19 +1,15 @@
-from flask import jsonify, session, render_template
+from flask import jsonify, session, make_response
 from backendHollow import app
 from backendHollow.forms import createCharacterForm
 import secrets
-
 
 @app.route("/csrf_token", methods = ["GET"])
 def csrf_token():
     token = secrets.token_hex(16)
     session['form_csrf_token'] = token
-    return jsonify({'csrfToken': token})
+    response = make_response(jsonify({'csrfToken': token}))
+    response.headers.add('Access-Control-Allow-Credentials', 'true')
+    return response
 
-
-# @app.route("/", methods = ['POST', 'GET'])
-# def index():
-#     # form = createCharacterForm()
-#     return render_template("index.html")
 
 from backendHollow.handlers import character, user
