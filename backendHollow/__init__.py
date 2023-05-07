7cbb2bfe4664154aa1aa9b07961eef880f37dce6
Sherman <shermanperez0@gmail.com>
2023-05-07 17:57:36 -0400

    sessions secure mode

--- backendHollow/__init__.py
+++ backendHollow/__init__.py
@@ -6,6 +6,7 @@ from datetime import timedelta
 
 app = Flask(__name__)
 app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(days=30)
+app.config['SESSION_COOKIE_SECURE'] = True
 CORS(app, supports_credentials=True)
 app.secret_key = 'de0b6d1205e578d7d79857e211e48182f8167878f4f6ad4b8cf6a7b447cab84c'
 app.config['MONGO_URI'] = "mongodb+srv://juokx1:ivhcJlPAiN1QnVsj@wikisherman.ewhecfi.mongodb.net/hollowDB?retryWrites=true&w=majority"
