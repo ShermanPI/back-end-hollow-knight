2489d3c0fd9cb2c1605c274b9d2d691a0600c498
Sherman <shermanperez0@gmail.com>
2023-05-07 18:21:04 -0400

    SESSION_COOKIE_HTTPONLY disabled

--- backendHollow/__init__.py
+++ backendHollow/__init__.py
@@ -8,6 +8,7 @@ import os
 app = Flask(__name__)
 app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(days=30)
 app.config['SESSION_COOKIE_SECURE'] = True
+app.config['SESSION_COOKIE_HTTPONLY'] = False
 CORS(app, supports_credentials=True)
 SECRET_KEY = os.urandom(32)
 app.config['SECRET_KEY'] = SECRET_KEY
