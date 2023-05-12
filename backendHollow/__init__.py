f8212fdb00f23f8a337f77418d1dd0eb5d8e2f1e
Sherman PI <shermanperez0@gmail.com>
2023-05-12 10:02:24 -0400

    cors with credentials added

--- backendHollow/__init__.py
+++ backendHollow/__init__.py
@@ -6,18 +6,18 @@ from datetime import timedelta
 import os
 
 app = Flask(__name__)
-CORS(app)
 
 @app.before_request
 def make_session_permanent():
     session.permanent = True
 
+SECRET_KEY = os.urandom(32)
+app.config['SECRET_KEY'] = SECRET_KEY
+CORS(app, supports_credentials=True)
 app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(days=30)
 app.config['SESSION_COOKIE_SECURE'] = True
 app.config['SESSION_COOKIE_HTTPONLY'] = False
 app.config['SESSION_COOKIE_SAMESITE'] = 'None'
-SECRET_KEY = os.urandom(32)
-app.config['SECRET_KEY'] = SECRET_KEY
 app.config['MONGO_URI'] = "mongodb+srv://juokx1:ivhcJlPAiN1QnVsj@wikisherman.ewhecfi.mongodb.net/hollowDB?retryWrites=true&w=majority"
 app.config['WTF_CSRF_ENABLED'] = False
 mongo = PyMongo(app)
